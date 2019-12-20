e39dacb67555fc04cc49d030d3202dcc6d218340
Christy Guan <cguan00@stuy.edu>
2019-12-20 10:50:44 -0500

    main file

---
+++ main.c
@@ -0,0 +1,7 @@
+// The main program should work as follows:
+//
+//     -Print a message that it will check if resources are available.
+//     -When they are, display the last line added to the story (or none, if it is new).
+//     -Prompt the user to enter the next line
+//     -Update the story
+//     -quit
