8523755d04ab10e67a5133bb1e0b648875602e9b
Christy Guan <cguan00@stuy.edu>
2019-12-20 11:03:38 -0500

    setting up files

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// A main writing program
+//
+//     Will attempt to access the resources using the semaphore
+//         Once in, it should display the last line added to the file (use shared memory to contain the size of the last line).
+//         Prompt the use for the next line.
+//         Once a new line is read, write that to the file, update the shared memory and then release the semaphore
+
 // The main program should work as follows:
 //
 //     -Print a message that it will check if resources are available.
@@ -5,3 +15,7 @@
 //     -Prompt the user to enter the next line
 //     -Update the story
 //     -quit
+
+int main(){
+  return 0;
+}
