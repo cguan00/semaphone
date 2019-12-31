14abc51ad34915c5c55c4fc129e67a9506604216
Christy Guan <cguan00@stuy.edu>
2019-12-31 12:00:12 -0500

    control headers

---
+++ control.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SHKEY 24601
+#define SEMKEY 24602
+#define SIZE 1000
+
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                              (Linux-specific) */
+};
+
+int runProgram();
+
+//command line argument: -c
+int creating();
+
+//command line argument: -r
+int removing();
+
+//command line argument: -v
+int viewing();
+
+int runProgram_write();
